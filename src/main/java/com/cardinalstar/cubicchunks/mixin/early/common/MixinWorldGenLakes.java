ff503664c1358670de9a79dddfda966056fbb53f
RecursivePineapple <recursive_pineapple@proton.me>
2026-05-28 14:15:10 -0400

    Patch MixinWorldGenLakes

--- src/main/java/com/cardinalstar/cubicchunks/mixin/early/common/MixinWorldGenLakes.java
+++ src/main/java/com/cardinalstar/cubicchunks/mixin/early/common/MixinWorldGenLakes.java
@@ -4,14 +4,15 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenLakes;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 
 @Mixin(WorldGenLakes.class)
 public class MixinWorldGenLakes {
@@ -23,9 +24,8 @@ public class MixinWorldGenLakes {
         return false;
     }
 
-    @ModifyVariable(method = "generate", at = @At("LOAD"), ordinal = 1, argsOnly = true)
-    public int fixHeight(int value, @Local(argsOnly = true) World world, @Local(argsOnly = true, ordinal = 0) int x,
-        @Local(argsOnly = true, ordinal = 2) int z) {
+    @Unique
+    private int cubicchunks$scanDownToSurface(int value, World world, int x, int z) {
         for (int i = 0; i < 16; i++) {
             if (!world.isAirBlock(x, value, z)) {
                 return value;
@@ -41,7 +41,10 @@ public class MixinWorldGenLakes {
     @Expression("y <= 4")
     @WrapOperation(method = "generate", at = @At("MIXINEXTRAS:EXPRESSION"))
     public boolean noopHeightCheck(int y, int four, Operation<Boolean> original, @Local(argsOnly = true) World world,
-        @Local(argsOnly = true, ordinal = 0) int x, @Local(argsOnly = true, ordinal = 2) int z) {
-        return world.isAirBlock(x, y, z);
+        @Local(argsOnly = true, ordinal = 0) int x, @Local(argsOnly = true, ordinal = 1) LocalIntRef yRef,
+        @Local(argsOnly = true, ordinal = 2) int z) {
+        int scannedY = this.cubicchunks$scanDownToSurface(y, world, x, z);
+        yRef.set(scannedY);
+        return world.isAirBlock(x, scannedY, z);
     }
 }
