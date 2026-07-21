193080383ed0733d6697ed28c9bdd143faaf1734
Jakob Erber <90445342+Jakfut@users.noreply.github.com>
2026-07-21 17:46:18 +0200

    Delay JourneyMap until surface cubes load (#87)
    * Delay JourneyMap until surface cubes load
    
    * Load JourneyMap compatibility mixin late

--- src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
+++ src/main/java/com/cardinalstar/cubicchunks/mixin/Mixins.java
@@ -319,7 +319,12 @@ public enum Mixins implements IMixins {
         new MixinBuilder("Route AE2 chunk section resends through the cubic synchronization protocol")
             .addCommonMixins("mod.MixinAE2Platform")
             .setPhase(Phase.EARLY)
-            .setApplyIf(() -> true))
+            .setApplyIf(() -> true)),
+    MIXIN_JOURNEYMAP_SURFACE_READY(new MixinBuilder("Delay JourneyMap updates until cubic surface data is loaded")
+        .addClientMixins("mod.MixinJourneyMapForgeHelper")
+        .setPhase(Phase.LATE)
+        .addRequiredMod(Mods.JourneyMap)
+        .setApplyIf(() -> true))
     //
     ;
 
